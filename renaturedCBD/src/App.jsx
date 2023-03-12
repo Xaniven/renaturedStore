518f7e1eb5250918143825ec9d4a04c216885e23
Xaniven <k.kelley5978@gmail.com>
2023-03-12 01:55:59 -0500

    start Navbar

--- renaturedCBD/src/App.jsx
+++ renaturedCBD/src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import "./App.scss";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ renaturedCBD/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className='w-[100%] h-[15vh] z-10 bg-slate-500'>
+      <h1>Re:NaturedCBD</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
