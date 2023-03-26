3ab2649c541011545b95303bdf29246ed49c4062
Xaniven <k.kelley5978@gmail.com>
2023-03-25 23:14:22 -0400

    add matching password check to new user form

--- renaturedCBD/src/components/NewUser.jsx
+++ renaturedCBD/src/components/NewUser.jsx
@@ -1,29 +1,73 @@
 import { useRef, useContext } from "react";
 import { FireContext } from "./Firebasecontext";
+import { useNavigate } from "react-router-dom";
+
 export default function NewUser() {
+  document.title = "New Account";
   const emailSignUp = useRef();
   const passSignUp = useRef();
   const { createNewUser } = useContext(FireContext);
+  const nav = useNavigate();
+
+  //check both passwordfields match
+  // refactor using onChange/useState for more responvive UX
+  function check_pass() {
+    console.log(document.getElementById("password").value);
+    if (
+      document.getElementById("password").value == document.getElementById("passwordConfirm").value
+    ) {
+      return true;
+    } else {
+      document.getElementById("password").classList.add("border-red-500");
+      document.getElementById("passwordConfirm").classList.add("border-red-500");
+      document.getElementById("passMessage").innerHTML = "Passwords Don't Match";
+      document.getElementById("passMessage2").innerHTML = "Passwords Don't Match";
+
+      return false;
+    }
+  }
   return (
     <div className='grid justify-items-center h-[85vh]'>
-      <div className=' bg-slate-600 h-fit bg-opacity-70'>
+      <div className=' bg-slate-600 h-fit bg-opacity-80 mt-8 rounded-xl shadow-2xl'>
+        <h1 className=' text-center text-4xl p-6'>Create a New Account</h1>
         <form
           onSubmit={(e) => {
             e.preventDefault();
-            console.log(emailSignUp.current.value);
-            createNewUser(emailSignUp.current.value, passSignUp.current.value);
+            if (check_pass() == true) {
+              createNewUser(emailSignUp.current.value, passSignUp.current.value);
+              nav("account");
+            } else {
+            }
           }}
           className='flex flex-col p-6 gap-4 w-[60vw]'
         >
-          <label for='email'>Email Address:</label>
+          <label htmlFor='email'>Email Address:</label>
           <input required ref={emailSignUp} type='email' name='email' id='email' />
 
-          <label for='password'>Create a Password:</label>
-          <input required ref={passSignUp} type='password' name='password' id='password' />
-          <label for='passwordConfirm'>Re-enter Password:</label>
-          <input required type='password' name='passwordConfirm' id='passwordConfirm' />
+          <label htmlFor='password' id='passMessage'>
+            Create a Password:
+          </label>
+
+          <input
+            required
+            className='border-2  appearance-none '
+            ref={passSignUp}
+            type='password'
+            name='password'
+            id='password'
+          />
+          <label htmlFor='passwordConfirm' id='passMessage2'>
+            Re-enter Password:
+          </label>
+          <input
+            required
+            className='border-2  appearance-none '
+            type='password'
+            name='passwordConfirm'
+            id='passwordConfirm'
+          />
           <div className=''>
-            <label for='checkbox'>
+            <label htmlFor='checkbox'>
               Click here to agree to{" "}
               <span className=' underline hover:text-green-800 hover:cursor-pointer p-2'>
                 Terms & Conditions.
@@ -33,7 +77,7 @@ export default function NewUser() {
           </div>
           <button
             type='submit'
-            className='opacity-100 m-4 border-black bg-green-800 hover:bg-green-600 border-2 p-2 px-6 rounded-lg  shadow-xl'
+            className='opacity-100 m-4 border-black bg-green-800 active:bg-green-800 hover:bg-green-600 border-2 p-2 px-6 rounded-lg  shadow-xl'
           >
             Create Account
           </button>
