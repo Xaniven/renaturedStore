173254ff53c1a23558041ab3584394ced974df1e
Xaniven <k.kelley5978@gmail.com>
2023-04-02 18:48:17 -0400

    finish store page & item component

--- renaturedCBD/src/components/ItemComp.jsx
+++ renaturedCBD/src/components/ItemComp.jsx
@@ -2,19 +2,35 @@ import React from "react";
 import logoR from "../img/logo-round.png";
 
 export default function ItemComp(props) {
+  let maxChar = 100;
+  let pargraph = props.props.description.substring(0, maxChar) + "...Read More";
+  let fullPara = props.props.description;
+
   return (
     <div
       key={props.key}
-      className='card flex flex-col justify-center justify-items-center w-fit h-fit border-2 border-black m-2 rounded-lg'
+      className='product-card flex flex-col justify-center justify-items-center min-w-80 h-auto max-w-auto border-2 border-black m-2  rounded-lg'
     >
-      <img src={props.props.images} height='200px' width='200px' alt='' />
-      <div className=' flex flex-col gap-2 bg-slate-400 rounded-lg text-left p-2 '>
-        <p className=' text-2xl font-extrabold'>item: {props.props.name}</p>
-        <p>{props.props.description}</p>
-        <p className=' text-sm text-green-800'>${props.props.prices[0].unit_amount}</p>
-        <button className='w-[100%] bg-green-700 hover:bg-green-800 py-2 mt-2 rounded-md'>
-          Add to Cart
-        </button>
+      <div className='product- image grid justify-center'>
+        <img src={props.props.images} height='256px' width='256px' alt='' />
+      </div>
+      <div className='product-desc flex flex-col justify-between min-h-[30vh] bg-slate-400 rounded-lg text-left p-2  '>
+        <p className=' text-2xl  font-extrabold h-fit '>item: {props.props.name}</p>
+        <p className='h-fit'>
+          <p className='block peer hover:opacity-0 hover:absolute h-fit'>
+            {fullPara.length <= maxChar ? fullPara : pargraph}
+          </p>
+          <p class=' hidden peer-hover:block h-fit'>{fullPara}</p>
+        </p>
+        <div className=''>
+          <p className='  text-lg text-green-800 text-end'>
+            ${(props.props.prices[0].unit_amount / 100).toFixed(2)}
+          </p>
+
+          <button className='w-[100%] bg-green-700 hover:bg-green-800 py-2 mt-2 rounded-md bottom-0 relative'>
+            Add to Cart
+          </button>
+        </div>
       </div>
     </div>
   );
--- renaturedCBD/src/pages/Storepage.jsx
+++ renaturedCBD/src/pages/Storepage.jsx
@@ -15,13 +15,32 @@ export default function Storepage() {
   }, []);
 
   return (
-    <div className='main-container container flex z-0 justify-center min-h-[100vh] max-w-[100vw] relative text-center'>
-      <div className='content container flex  p-8 m-24 min-w-[95vw] min-h-[auto] bg-amber-900 rounded-xl border-[3px] border-green-800 opacity-90 shadow-xl shadow-slate-700'>
-        {store.map((item) => (
-          <ItemComp props={item} key={item.id} />
-        ))}
-        {/* <ItemComp />; */}
+    <>
+      <h1 className='lg:text-6xl md:text-4xl text-2xl text-center mt-8 p-2 bg-slate-600 rounded-3xl'>
+        <span className='mr-8'>
+          <span className='text-green-700'>Re</span>
+          <span className=' text-amber-900'>
+            Natured
+            <span className=' absolute text-3xl'>™</span>
+          </span>
+        </span>
+        Shop
+      </h1>
+      <div className='main-container container flex z-0 justify-center min-h-[100vh] max-w-[100vw] relative text-center'>
+        <div className='content container p-2 m-24 min-w-[95vw] h-[85vh] bg-amber-900 rounded-xl border-[3px] border-green-800 opacity-90 shadow-xl shadow-slate-700 overflow-y-scroll'>
+          <div className='grid p-2 m b-8 justify-center md:grid-cols-2  lg:grid-cols-4 w-[100%] h-[100%]'>
+            {store.map((item) => (
+              <ItemComp props={item} key={item.id} />
+            ))}
+            {store.map((item) => (
+              <ItemComp props={item} key={item.id} />
+            ))}
+            {store.map((item) => (
+              <ItemComp props={item} key={item.id} />
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
