5240e4307e659deb37f0842d369c4ea94d40fcce
Xaniven <k.kelley5978@gmail.com>
2023-04-16 23:12:14 -0400

    spine fixes

--- renaturedCBD/src/pages/Homepage.jsx
+++ renaturedCBD/src/pages/Homepage.jsx
@@ -5,7 +5,7 @@ function Homepage() {
   document.title = "Re:Natured!";
   return (
     <div className='h-auto max-w-[100vw] flex  '>
-      <div className='left-side w-0 lg:w-[40vw] min-h-[155vh] lg:ml-8 lg:p-4 lg:m-6 flex-row justify-evenly z-20 lg:z-0'>
+      <div className='left-side w-0 lg:w-[60vw] min-h-[155vh] lg:ml-8 lg:p-4 lg:m-6 flex-row justify-evenly z-20 lg:z-0'>
         <section className='flex justify-evenly relative z-20 lg:z-0 p-6 my-6 min-h-[60vh] w-[100vw] lg:w-[93vw] shadow-2xl bg-slate-600  bg-opacity-80  border-green-800 border-4 rounded-2xl '>
           <div className='w-[100%] h-[100%] text-center lg:text-left'>
             <h1 className='relative text-6xl lg:text-8xl mb-8'>
@@ -39,7 +39,7 @@ function Homepage() {
             </div>
           </div>
         </section>
-        <section className=' relative z-20 p-8 my-6 h-[fit] w-[96.5vw] lg:w-[60vw] shadow-2xl bg-slate-500 bg-opacity-80 border-green-800 border-4 rounded-2xl '>
+        <section className=' relative z-20 p-8 my-6 h-[fit] w-[99vw] lg:w-[60vw] shadow-2xl bg-slate-500 bg-opacity-80 border-green-800 border-4 rounded-2xl '>
           <h1 className=' text-4xl mb-8 z-20'>What Are Terpenes?</h1>
           <p className='w-[45vw] text-xl'>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea possimus quod eum molestias
@@ -47,7 +47,7 @@ function Homepage() {
             possimus fugiat sint explicabo quas totam eum.
           </p>
         </section>
-        <section className=' relative z-20 p-8 my-6 h-[fit] w-[96.5vw] lg:w-[60vw] shadow-2xl bg-slate-500 bg-opacity-80 border-green-800 border-4 rounded-2xl '>
+        <section className=' relative z-20 p-8 my-6 h-[fit] w-[99vw] lg:w-[60vw] shadow-2xl bg-slate-500 bg-opacity-80 border-green-800 border-4 rounded-2xl '>
           <h1 className=' text-4xl mb-8 z-20'>"Terp Brew" (Terpene Infused Coffees)</h1>
           <p className='w-[45vw] text-xl'>
             With both caffinated and decafe blends, we strive to bring somthing new to your morning
@@ -55,7 +55,7 @@ function Homepage() {
             brew, bringing a bright way to start the day.
           </p>
         </section>
-        <section className='lg:absolute relative z-20 p-8 my-6 h-[fit] w-[96.5vw] lg:w-[60vw] shadow-2xl bg-slate-500 bg-opacity-80 border-green-800 border-4 rounded-2xl '>
+        <section className='lg:absolute relative z-20 p-8 my-6 h-[fit] w-[99vw] lg:w-[60vw] shadow-2xl bg-slate-500 bg-opacity-80 border-green-800 border-4 rounded-2xl '>
           <h1 className=' text-4xl mb-8'>Terpene Infused Teas</h1>
           <p className='z-20 w-[45vw] text-xl'>
             Our teas are rich in terpenes to take the flavour expirence of your tea to the next
@@ -64,11 +64,8 @@ function Homepage() {
           </p>
         </section>
       </div>
-      <div className='right-side top-0  sticky w-[100%] h-[85vh] z-1 lg:z-10'>
-        <spline-viewer
-          loading-anim
-          url='https://prod.spline.design/4ywhzUu7rYokb2Bv/scene.splinecode'
-        ></spline-viewer>
+      <div className='right-side top-0  sticky lg:w-[40vw] h-[100vh] lg:h-[85vh] z-1 lg:z-10'>
+        <spline-viewer url='https://prod.spline.design/PibOZ9DNXlOJ2g2n/scene.splinecode'></spline-viewer>
       </div>
     </div>
   );
